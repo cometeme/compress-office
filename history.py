6918182553ddbe229b67b6236687066f65efcc88
cometeme <22635759+cometeme@users.noreply.github.com>
2022-06-02 13:47:30 +0800

    use modify time instead of md5

--- history.py
+++ history.py
@@ -1,17 +1,16 @@
 import csv
-from hashlib import md5
 from os.path import abspath, exists
-
+import os
 
 class history:
     def __init__(self, path: str) -> None:
         self.path = path
-        self.history = dict()  # <file_path: file_md5>
+        self.history = dict()  # <file_path: modify_time>
         if exists(self.path):
             with open(self.path, "r") as f:
                 reader = csv.reader(f)
                 for row in reader:
-                    self.history[row[0]] = row[1]
+                    self.history[row[0]] = int(row[1])
 
     def __del__(self) -> None:
         with open(self.path, "w") as f:
@@ -21,31 +20,21 @@ class history:
 
     def add_history(self, file_path: str) -> None:
         file_path = abspath(file_path)
-        # Get file's md5
-        with open(file_path, "rb") as f:
-            m = md5()
-            m.update(f.read())
-            file_md5 = m.hexdigest()
+        mtime = os.stat(file_path).st_mtime_ns
 
         # Update history
-        self.history[file_path] = file_md5
+        self.history[file_path] = mtime
 
-    def file_in_history(self, file_path: str, check_md5: bool) -> bool:
+    def file_in_history(self, file_path: str) -> bool:
         file_path = abspath(file_path)
         # Search from history
         if file_path not in self.history:
             return False
 
-        if not check_md5:
-            return True
+        # Get file's modify_time
+        mtime = os.stat(file_path).st_mtime_ns
 
-        # Get file's md5
-        with open(file_path, "rb") as f:
-            m = md5()
-            m.update(f.read())
-            file_md5 = m.hexdigest()
-
-        return file_md5 == self.history[file_path]
+        return mtime == self.history[file_path]
 
     def clean_up(self) -> None:
         clean_up_list = []
